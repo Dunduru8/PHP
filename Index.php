09fd6821d9e581e187f446e8eac28ea6c1c75c7a
dunduru8 <dunduru1@gmail.com>
2019-04-27 09:13:49 +0300

    4 задание

--- Index.php
+++ Index.php
@@ -1,35 +1,20 @@
-<!doctipe html>
-<html lang = "en"></html>
-<head>
-     <meta charset = "UTF-8">
-     <meta mane = "vieport" content = "width = device-width, user-scalable = no, initial-scale = 1.0, maximum-scale = 1.0, minimum-scale = 1.0">
-     <meta http-equiv="X-UA-Compatible" content = "ie=edge">
-     <title>Document</title>
-</head>
-   
-     <body>
-    <?php echo "Hello, world!" ;
-    $a = 5;
-    $b = '05';
-    var_dump($a == $b);         //true потому что после приведения типов переменные равны 
-    var_dump((int)'012345');     // потому что  int это тип данных integer для целых чисел
-    var_dump((float)123.0 === (int)123.0); // потому что === типы тоже должны быть равны
-    var_dump((int)0 === (int)'hello, world'); // true потому что операторы == и! = не сравнивают тип. «Hello world» автоматически преобразуется в целое число, равное 0 
-    
-    $x = 1;
-    $y = 2;
-    echo ("<p><b>Начальное значение:</b> ");
-         var_dump($x);
-         var_dump($y);
-         echo ("</p>");
-         $y = $y - $x;  //y = 1
-         $x = $x + $y;   // x = 2;
-         
-    echo ("<p><b>Конечное значение:</b>  ");         
-         var_dump($x);
-         var_dump($y);
-         echo ("</p>");
-   ?>
-     
-    </body>
-     
+<?php
+         $content = "<h2> Задание 4.</h2>";
+         $day = date("d.m.Y");
+$html = <<<php
+      <!doctipe html>
+      <html lang = "en"></html>
+      <head>
+      <meta charset = "UTF-8">
+      <meta mane = "vieport" content = "width = device-width, user- scalable = no, initial-scale = 1.0, maximum-scale = 1.0,  minimum-scale = 1.0">
+      <meta http-equiv="X-UA-Compatible" content = "ie=edge">
+      <title>Document</title>
+      </head>
+      <body>
+        {$content}
+       <h3>{$day}</h3>
+      </body>
+      </html>
+      php;
+echo $html;
+?>   
