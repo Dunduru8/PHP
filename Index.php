e2404ea37c6659eec5446faa80f99cb26c90472c
dunduru8 <dunduru1@gmail.com>
2019-05-06 20:15:44 +0300

    Не сделан только 10 пункт

--- Index.php
+++ Index.php
@@ -8,143 +8,169 @@
  </head>
 <body>
 
- <?php
-    $a = (random_int(-1000, 999));
-    $b = (random_int(-1000, 999));
-    echo ("<p><b>Pандомное значение: </b> ");
+<?php
+// 6 задание
     
-      var_dump($a);
-      var_dump($b);
+    $menu = [
+     "home",
+     "men" =>["shirt", "jacket", "T-shirt"],
+     "women" => ["dress", "top", "skirt", "shoes"],
+     "children" => ["dress", "top", "coat", "toys"]
+    ];    
     
-    echo ("</p><hr>");
-    if($a >= 0 && $b >= 0){
-        echo ("<p><b>" . $d = $a - $b . "</b></p>");
-    }else if($a < 0 && $b < 0){
-       echo ("<p><b>" . $d = $a * $b . "</b></p>"); 
-    }else if(($a < 0 && $b >= 0) ||($a >= 0 && $b < 0)){
-        echo ("<p><b>" . $d = $a + $b . "</b></p>"); 
-    };
-    
-    $a = (random_int(0, 15));
-    var_dump($a);
-   
-    echo ("<p><b>Рандомное число = " . $a . ". Вывод числел начиная с рандомного числа до 15: ");
-    switch($a){
-        case "1": 
-        echo ("<p>" . $a++ . "</p>");
-        
-        case "2": 
-         echo ("<p>" . $a++ . "</p>");
-        
-        case "3": 
-         echo ("<p>" . $a++ . "</p>");
+foreach ($menu as $key => $value) {
+     
+    if(! is_array ($value)){
+       echo "<ul><b>" . $value . "</ul></b>";
+         continue;
+    }else
        
-        case "4": 
-         echo ("<p>" . $a++ . "</p>");
-       
-        case "5": 
-         echo ("<p>" . $a++ . "</p>");
-       
-        case "6": 
-         echo ("<p>" . $a++ . "</p>");
-        
-        case "7": 
-         echo ("<p>" . $a++ . "</p>");
-        
-        case "8": 
-         echo ("<p>" . $a++ . "</p>");
-        
-        case "9": 
-         echo ("<p>" . $a++ . "</p>");
-        
-        case "10": 
-         echo ("<p>" . $a++ . "</p>");
-        
-        case "11": 
-         echo ("<p>" . $a++ . "</p>");
-        
-        case "12": 
-         echo ("<p>" . $a++ . "</p>");
-        
-        case "13": 
-         echo ("<p>" . $a++ . "</p>");
-        
-        case "14": 
-         echo ("<p>" . $a++ . "</p>");
-        
-        case "15": 
-         echo ("<p>" . $a++ . "</p>");
-        break;
-    };
-    
-    function summ($x, $y){
-        return $d = $x + $y;
-    };
-    
-    function difference($x, $y){
-        return $d = $x - $y;
-    };
-    function mult($x, $y){
-        return $d = $x * $y;
-    }
-    function division($x, $y){
-        return $d = $x / $y;
-    };
-    
-   $arg1 = 23;
-   $arg2 = 12;      
-   $operation = "*";    
-       
-       
-       
-    function mathOperation($arg1, $arg2, $operation){
-        switch($operation){
-            case "+":
-            echo ("<hr> Результат функции mathOperation " . summ($arg1, $arg2) . "<hr>");
-            break;
-                
-            
-            case "-":
-            echo ("<hr> Результат функции mathOperation " . difference($arg1, $arg2) . "<hr>");
-            break;
-            
-            case "*":
-            echo ("<hr> Результат функции mathOperation " . mult($arg1, $arg2) ."<hr>");
-            break;
-                
-            case "/":
-            echo ("<hr> Результат функции mathOperation " . division($arg1, $arg2) . "<hr>");
-            break;    
+       echo "<ul><b>" . $key . "</b>";
+ 
+        foreach ($value as $key => $options) {
+          if($options == end($value)){
+           echo  "<li>" . $options . "</li></ul>";   
+          }else  
+           echo  "<li>" . $options . "</li>";
         }
-    };
-mathOperation($arg1, $arg2, $operation);
-    
-  
- $val = 5;
- $pow = 3;   
+     
+};        
     
     
- function power($val, $pow){
-  if($pow == 0){
-    return 1;
+ //1. задание
+    
+$a=0;
+   while($a++ < 100) if($a % 3 == 0) 
+   echo $a." ";
+
+
+//2. задание
+    
+$b = 10;
+$i = 0;
+function pow1($i) {
+  if ($i == 0) {
+    return  "<hr>" . "$i" . " - это ноль <br/>" ;
   }
-  if($pow < 0){
-    return power( 1/$val, -$pow); 
+  if ($i % 2) {
+    return "$i" . " - нечетное число <br/>";
+  } else {
+    return "$i" . " - четное число <br/>";
   }
-  return $val * power($val, $pow-1); // вызов функции внутри функции
 }
  
-echo ("Результат функции возведения в степень " .power($val, $pow) . "<hr>"); 
+do {
+  echo pow1($i);
+  $i++;
+} while ($i <= $b);   
+    
+    
+
+// 3. задание   
+    
+$obl = [
+    "Московская область" => [
+        "Москва", "Зеленоград", "Клин", "Звенигород", "Коломна", "Алабино", "Барвиха"
+    ],
+    "Ленинградская область" => [
+        "Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт", "Выборг", "Алексеевка", "Беседа"
+    ],
+    "Рязанская область" => [
+        "Касимов", "Сасово", " Скопин", "Рязань", " Кораблино", "Берестянки", "Вышгород", "Ключ", "Аманово"
+    ]
+];
+    
+    
+foreach ($obl as $key => $value) {
+ echo "<br> <b>" . $key . ":<br> </b>";
+ 
+    foreach ($value as $key => $value) {
+      echo $value . ", ";
+     }
+};    
+    
+
+// 8. задание 
+
+echo "<hr>";
+    
+foreach ($obl as $key => $value) {
+   echo "<br> <b>" . $key . ":<br> </b>";
+ 
+  foreach ($value as $key => $value) {
+     if(mb_substr($value, 0, 1) == "К") {
+        echo $value . ", ";
+        }
+    }
+};
+echo "<hr>";  
+    
+    
+//7. задание
+
+for ($j = 0; $j <= 9; print $j++ . ","){};    
+   
+    
+//4., 5, 9 задание 
+    
+    
+function rus2translit($string) {
+    $conv = array(
+        "а" => "a",   "б" => "b",   "в" => "v",
+        "г" => "g",   "д" => "d",   "е" => "e",
+        "ё" => "ye",  "ж" => "zh",  "з" => "z",
+        "и" => "i",   "й" => "y",   "к" => "k",
+        "л" => "l",   "м" => "m",   "н" => "n",
+        "о" => "o",   "п" => "p",   "р" => "r",
+        "с" => "s",   "т" => "t",   "у" => "u",
+        "ф" => "f",   "х" => "h",   "ц" => "c",
+        "ч" => "ch",  "ш" => "sh",  "щ" => "sch",
+        "ь" => "\'",  "ы" => "y",   "ъ" => "\'",
+        "э" => "e",   "ю" => "yu",  "я" => "ya",
+        
+        "А" => "A",   "Б" => "B",   "В" => "V",
+        "Г" => "G",   "Д" => "D",   "Е" => "E",
+        "Ё" => "Ye",  "Ж" => "Zh",  "З" => "Z",
+        "И" => "I",   "Й" => "Y",   "К" => "K",
+        "Л" => "L",   "М" => "M",   "Н" => "N",
+        "О" => "O",   "П" => "P",   "Р" => "R",
+        "С" => "S",   "Т" => "T",   "У" => "U",
+        "Ф" => "F",   "Х" => "H",   "Ц" => "C",
+        "Ч" => "Ch",  "Ш" => "Sh",  "Щ" => "Sch",
+        "Ь" => "\'",  "Ы" => "Y",   "Ъ" => "\'",
+        "Э" => "E",   "Ю" => "Yu",  "Я" => "Ya",
+    );
+    return strtr($string, $conv);
+}
+    
+function str2url($str) {
+    
+    // переводим в транслит
+    $str = rus2translit($str);
+    
+    // заменям " " на "_"
+    $str = preg_replace("/\s+/", "_", $str);
+   
+    return $str;
+}    
+ 
+ ?>    
+
+<h3>Строка для транслитерации:</h3>
+<form name = "form" method = "get"> 
+<input name = "str" type = "text" style = "width: 300px">
+</form>          
+
+<?php         
+$value = $_GET["str"];
+$modified_value =  str2url($value);  
+echo "<p>Введенное значение</p>" . $value . "<br>";
+echo "<p>Измененное значение</p>" . $modified_value . "<br>";     
     
 ?>   
-    
+ 
      
-             
-  <div >
-    <?php 
-    echo ("Текущий год ");  
-    echo date("Y"); ?>  
-  </div>    
 </body>
 
-</html>   
+</html> 
+  
